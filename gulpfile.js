754789a59369b0f2811c12a9f2ba43e2b2365dde
Madhawa Ariyarathna <madhawa.ariyarathna@pearson.com>
2017-12-30 23:49:36 +0530

    Copy Javascript files to public folder using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const gulpClean = require('gulp-clean');
 const publicFolder = "server/public";
 const sourceFolder = "client";
+const bowerComponentFolder = "bower_components";
 
 gulp.task('clean', () => {
 
@@ -9,10 +10,24 @@ gulp.task('clean', () => {
         .pipe(gulpClean({force: true}));
 });
 
-gulp.task('copy', ['clean'], () => {
+gulp.task('copyHtml', ['clean'], () => {
 
     return gulp.src(sourceFolder + '/index.html')
         .pipe(gulp.dest(publicFolder));
 });
 
-gulp.task('build', ['copy']);
+gulp.task('copyJavaScript', ['clean'], () => {
+
+    gulp.src(sourceFolder + '/js/*')
+        .pipe(gulp.dest(publicFolder + '/js'));
+});
+
+gulp.task('copyBowerComponents', ['clean'], () => {
+
+    gulp.src([
+        bowerComponentFolder + '/vue/dist/vue.js'
+    ])
+        .pipe(gulp.dest(publicFolder + '/js'));
+});
+
+gulp.task('build', ['copyHtml', 'copyJavaScript', 'copyBowerComponents']);
